8a1f1fb1c5c90a72d274f43eaa55ae820131e62f
yulong1902 <38685502+yulong1902@users.noreply.github.com>
2018-05-27 22:28:48 +0800

    Add files via upload

---
+++ 我的js/js1.js
@@ -0,0 +1,8 @@
+function show(li){
+    var ul=li.getElementsByTagName("a02");
+    ul.style.display="block";
+}
+function hide(li){
+    var ul=li.getElementsByTagName("a02");
+    ul.style.display="none";
+}
